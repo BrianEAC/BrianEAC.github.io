4417542c0f524395e65d180bbe4cc10704292af6
Brian Ataria Ceraldi <brianatariaceraldi@gmail.com>
2021-05-01 20:13:58 -0300

    added change size funcionality

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
 let cell = [];
+let x = 16;
 const container = document.querySelector("#container");
-container.style.gridTemplateColumns = ("repeat(16), 1fr");
-container.style.gridTemplateRows = ("repeat(16), 1fr");
 
-let numberOfCells = 16 * 16
+let cells = document.querySelectorAll(".cells")
+let numberOfCells = x ** 2;
 
-
-for(let i = 0; i < numberOfCells; i++) {
-    cell[i] = document.createElement("div");
-    cell[i].classList.add('cells');
-    container.appendChild(cell[i]);
+function createGrid() {
+  for(let i = 0; i < numberOfCells; i++) {
+      cell[i] = document.createElement("div");
+      cell[i].classList.add('cells');
+      container.appendChild(cell[i]);
+  };
+  cells = document.querySelectorAll(".cells")
+  cells.forEach((item) => {
+  item.addEventListener('mouseenter', (e) => {
+      e.target.style.backgroundColor = 'black'
+  })
+})
 }
 
-const cells = document.querySelectorAll(".cells");
+function removeGrid() {
+  while (container.innerHTML) {
+    container.innerHTML = '';
+  }
+}
 
-cells.forEach((item) => {
-    item.addEventListener('mouseenter', (e) => {
-        e.target.style.backgroundColor = 'black'
-    })
-})
+function setNewSize() {
+ container.style.gridTemplateColumns = `repeat(${x}, 1fr)`
+ container.style.gridTemplateRows = `repeat(${x}, 1fr)`
+}
 
 const button = document.querySelector('#clear');
 
+
+
+createGrid()
 button.addEventListener('click', () =>{
   cells.forEach((item) => {
       if (item.style.backgroundColor = 'black' ) {
         item.style.backgroundColor = '#DECAB0'; 
       }
+      
   })
+  do x = prompt('grid size? (1-90): ')
+  while(x>90 ||x<1);
+  numberOfCells = x * x; 
+  setNewSize();
+  removeGrid(); 
+  createGrid();
     
 })
-
 
